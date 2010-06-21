5b79eafaff02e34df7cb9f0c9a570b82ac948e00
Tomo Matsumoto <tomoyuki28jp@gmail.com>
2010-06-21 23:34:12 +0900

    First commit

---
+++ google_lauguage_code_swi.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name           Google Lauguage Code Switcher
+// @namespace      Minibuffer.GLCS
+// @description    Change/add the hl parameter value on google result pages.
+// @include        http://*
+// ==/UserScript==
+
+var VERSION = '22 Feb 2010';
+
+if (window.Minibuffer) {
+    (function () {
+        var _hl = function(lang) {
+            if (location.search.match(/hl=([a-zA-Z]{2})/)) {
+                location.href = location.href.replace(/hl=([a-zA-Z]{2})/, 'hl='+lang)
+            } else {
+                location.href += '&hl='+lang
+            }
+        }
+
+        Minibuffer.addCommand({
+            'ja': function() { _hl('ja') },
+            'en': function() { _hl('en') }
+        });
+    })()
+}
